3e8fbad9cb8fc183076e995a166f8efcf2170fa7
Naoto Mizuno <mizuno@eps.s.u-tokyo.ac.jp>
2017-12-01 19:51:21 +0900

    partition

--- run.py
+++ run.py
@@ -4,55 +4,89 @@ import numpy
 import cupy_perf
 
 
-class Perf1(cupy_perf.PerfCases):
-    enable_line_profiler = False
+class Perf(cupy_perf.PerfCases):
+    def setUp(self, n, k):
+        self.n = n
+        self.k = k
+        self.a = cupy.arange(self.n).astype('f')
+        self.orig = self.a.copy()
+        cupy.random.shuffle(self.a)
 
+    def tearDown(self):
+        self.a = cupy.partition(self.a, self.k)
+        assert numpy.array_equal(cupy.asnumpy(cupy.sort(self.orig)), cupy.asnumpy(cupy.sort(self.a)))
+        assert cupy.max(self.a[:self.k]) <= self.a[self.k]
+        assert cupy.min(self.a[self.k:]) >= self.a[self.k]
+
+
+class Perf_12_20(Perf):
     def setUp(self):
-        shape_tiny = (2, 1, 2)
-        shape_huge = (2000, 50, 100)
-        self.a = cupy.empty(shape_tiny, numpy.float32)
-        self.b = cupy.empty(shape_tiny, numpy.float32)
-        self.c = cupy.empty(shape_tiny, numpy.float32)
-        self.a_huge = cupy.empty(shape_huge, numpy.float32)
-        self.b_huge = cupy.empty(shape_huge, numpy.float32)
-        self.c_huge = cupy.empty(shape_huge, numpy.float32)
+        print(self.__class__.__name__)
+        super(Perf_12_20, self).setUp(1 << 12, 20)
 
-        self.kernel = self._get_kernel()
+    @cupy_perf.attr(n=100)
+    def perf_partition(self):
+        cupy.partition(self.a, self.k)
 
-    def _get_kernel(self):
-        return cupy.ElementwiseKernel(
-            '''T a, T b''', '''T c''',
-            '''c = a + b;''', 'test_kernel')
 
-    def perf_empty(self):
-        pass
+class Perf_16_20(Perf):
+    def setUp(self):
+        print(self.__class__.__name__)
+        super(Perf_16_20, self).setUp(1 << 16, 20)
 
-    def perf_sum(self):
-        cupy.sum(self.a)
+    @cupy_perf.attr(n=100)
+    def perf_partition(self):
+        cupy.partition(self.a, self.k)
 
-    @cupy_perf.attr(n=500)
-    def perf_sum_huge(self):
-        a = cupy.sum(self.a_huge)
-        assert a.shape == ()
 
-    def perf_add(self):
-        cupy.add(self.a, self.b)
+class Perf_20_20(Perf):
+    def setUp(self):
+        print(self.__class__.__name__)
+        super(Perf_20_20, self).setUp(1 << 20, 20)
 
-    def perf_add_out(self):
-        cupy.add(self.a, self.b, out=self.c)
+    @cupy_perf.attr(n=100)
+    def perf_partition(self):
+        cupy.partition(self.a, self.k)
 
-    def perf_add_out_huge(self):
-        cupy.add(self.a_huge, self.b_huge, out=self.c_huge)
 
-    def perf_userkernel_create(self):
-        self._get_kernel()
+class Perf_24_20(Perf):
+    def setUp(self):
+        print(self.__class__.__name__)
+        super(Perf_24_20, self).setUp(1 << 24, 20)
 
-    def perf_userkenel(self):
-        self.kernel(self.a, self.b, self.c)
+    @cupy_perf.attr(n=100)
+    def perf_partition(self):
+        cupy.partition(self.a, self.k)
 
-    @cupy_perf.attr(n=10000)
-    def perf_userkernel_huge(self):
-        self.kernel(self.a_huge, self.b_huge, self.c_huge)
+
+class Perf_24_10(Perf):
+    def setUp(self):
+        print(self.__class__.__name__)
+        super(Perf_24_10, self).setUp(1 << 24, 10)
+
+    @cupy_perf.attr(n=100)
+    def perf_partition(self):
+        cupy.partition(self.a, self.k)
+
+
+class Perf_24_100(Perf):
+    def setUp(self):
+        print(self.__class__.__name__)
+        super(Perf_24_100, self).setUp(1 << 24, 100)
+
+    @cupy_perf.attr(n=100)
+    def perf_partition(self):
+        cupy.partition(self.a, self.k)
+
+
+class Perf_24_1000(Perf):
+    def setUp(self):
+        print(self.__class__.__name__)
+        super(Perf_24_1000, self).setUp(1 << 24, 1000)
+
+    @cupy_perf.attr(n=100)
+    def perf_partition(self):
+        cupy.partition(self.a, self.k)
 
 
 cupy_perf.run(__name__)
