c6f9aa8542beeda2116017ca5d4190a345764c17
Naoto Mizuno <mizuno@eps.s.u-tokyo.ac.jp>
2018-03-30 17:58:39 +0900

    Add OpenCV

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import cupy
 import cupyx.scipy.ndimage
+import cv2
 import numpy
 import scipy.ndimage
 
@@ -12,10 +13,10 @@ class Perf(cupy_perf.PerfCases):
         self.a = cupy.empty(shape, numpy.float32)
 
     def affine(self):
-        cupyx.scipy.ndimage.affine_transform(self.a, [[1., 0., 0., -2.5], [0., 0.5, 0.0, 0.0], [0., 0., 1., 0.]], order=1)
+        cupyx.scipy.ndimage.affine_transform(self.a, cupy.array([[1., 0., 0., -2.5], [0., 0.5, 0., 0.], [0., 0., 1., 0.]]), order=1)
 
     def rotate(self):
-        cupyx.scipy.ndimage.rotate(self.a, 1, order=1)
+        cupyx.scipy.ndimage.rotate(self.a, 1, order=1, reshape=False)
 
     def zoomin(self):
         cupyx.scipy.ndimage.zoom(self.a, [2, 2, 1], order=1)
@@ -30,16 +31,43 @@ class Perf_cpu(cupy_perf.PerfCases):
         self.a = numpy.empty(shape, numpy.float32)
 
     def affine(self):
-        scipy.ndimage.affine_transform(self.a, [[1., 0., 0., -2.5], [0., 0.5, 0.0, 0.0], [0., 0., 1., 0.]], order=1)
+        scipy.ndimage.affine_transform(self.a, [[1., 0., 0., -2.5], [0., 0.5, 0., 0.], [0., 0., 1., 0.]], order=1)
 
     def rotate(self):
-        scipy.ndimage.rotate(self.a, 1, order=1)
+        scipy.ndimage.rotate(self.a, 1, order=1, reshape=False)
 
     def zoomin(self):
         scipy.ndimage.zoom(self.a, [2, 2, 1], order=1)
 
     def zoomout(self):
         scipy.ndimage.zoom(self.a, [0.5, 0.5, 1], order=1)
+
+
+class Perf_opencv(cupy_perf.PerfCases):
+    def setUp(self, n, sz):
+        shape = (sz, sz)
+        self.a = numpy.empty(shape, numpy.float32)
+        cv2.ocl.setUseOpenCL(True)
+
+    def affine(self):
+        M = numpy.array([[2., 0., 0.], [0., 1., 5.]])
+        cv2.warpAffine(self.a, M, self.a.shape[:2])
+
+    def rotate(self):
+        M = cv2.getRotationMatrix2D((self.a.shape[1] / 2.0 - 0.5, self.a.shape[0] / 2.0 - 0.5), 1, 1)
+        cv2.warpAffine(self.a, M, self.a.shape[:2])
+
+    def zoomin(self):
+        shape = list(self.a.shape)[:2]
+        shape[0] *= 2
+        shape[1] *= 2
+        cv2.resize(self.a, tuple(shape))
+
+    def zoomout(self):
+        shape = list(self.a.shape)[:2]
+        shape[0] //= 2
+        shape[1] //= 2
+        cv2.resize(self.a, tuple(shape))
 
 
 class Perf_100_100(Perf):
@@ -68,6 +96,28 @@ class Perf_100_100_cpu(Perf_cpu):
     def setUp(self):
         print(self.__class__.__name__)
         super(Perf_100_100_cpu, self).setUp(100, 100)
+
+    @cupy_perf.attr(n=100)
+    def perf_affine(self):
+        self.affine()
+
+    @cupy_perf.attr(n=100)
+    def perf_rotate(self):
+        self.rotate()
+
+    @cupy_perf.attr(n=100)
+    def perf_zoomin(self):
+        self.zoomin()
+
+    @cupy_perf.attr(n=100)
+    def perf_zoomout(self):
+        self.zoomout()
+
+
+class Perf_100_100_opencv(Perf_opencv):
+    def setUp(self):
+        print(self.__class__.__name__)
+        super(Perf_100_100_opencv, self).setUp(100, 100)
 
     @cupy_perf.attr(n=100)
     def perf_affine(self):
